1e2f9f58ae4f0b543b713e8516b57d178185252e
Alvaro Castro <arucastro@gmail.com>
2025-04-03 19:46:35 +0200

    Last round of exercises Variables and Basic Operations.

---
+++ VariablesAndBasicOperations3/src/Exercise1P.java
@@ -0,0 +1,14 @@
+public class Exercise1P {
+    public static void main(String[] args) {
+//        EXERCISE 1P
+//        Two trains depart from different cities (we know the distance between the
+//        2 cities) in the same direction, but in opposite directions. Make a program that
+//        calculates the point of collision of both locomotives knowing the speed
+//        (we assume constant) of each of them. You must calculate and display
+//        d1 and d2. Then make use of the following formulas:
+
+
+
+
+    }
+}
