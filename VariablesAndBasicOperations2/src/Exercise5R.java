4acc2ce30363f24d0c1c1a1d19187a685c9c4724
Alvaro Castro <arucastro@gmail.com>
2025-04-03 18:05:55 +0200

    5R Done

--- VariablesAndBasicOperations2/src/Exercise5R.java
+++ VariablesAndBasicOperations2/src/Exercise5R.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Exercise5R {
     public static void main(String[] args) {
 //        EXERCISE 5R
@@ -6,5 +8,27 @@ public class Exercise5R {
 //        part is worth 50% and the theoretical part 40%. Make a program will
 //        read the name of a student and his three marks and will show
 //        Final mark for [NAME] is [FINAL MARK].
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the name: ");
+        String name = scanner.next();
+
+        System.out.println("Enter the practical marks: ");
+        int practicalPart = scanner.nextInt();
+
+        System.out.println("Enter the theoretical marks: ");
+        int theoreticalPart = scanner.nextInt();
+
+        System.out.println("Enter the problem marks: ");
+        int problemPart = scanner.nextInt();
+
+        double totalMarks = practicalPart * 0.10 +
+                problemPart * 0.50 +
+                theoreticalPart * 0.40;
+
+        System.out.println("Final mark for " + name + " is " + totalMarks);
+
     }
 }
