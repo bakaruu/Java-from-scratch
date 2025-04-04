46fe674dd99e4b5b65f881a3d384866e9224af7d
Alvaro Castro <arucastro@gmail.com>
2025-04-04 18:16:57 +0200

    Exercise1A_IF_ELSEIF_ELSE DONE

--- 2_Control flow statements_1/src/Exercise1A_IF_ELSEIF_ELSE.java
+++ 2_Control flow statements_1/src/Exercise1A_IF_ELSEIF_ELSE.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Exercise1A_IF_ELSEIF_ELSE {
     public static void main(String[] args) {
+
+//        EXERCISE 1A
+//        Develop a program that asks for two numbers and shows
+//        whether they are equal or not.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int x = scanner.nextInt();
+
+        System.out.println("Enter number 2: ");
+        int j = scanner.nextInt();
+
+        if(x == j){
+            System.out.println("The numbers are equals");
+        }else{
+            System.out.println("The numbers are different");
+
+        }
 
     }
 }
