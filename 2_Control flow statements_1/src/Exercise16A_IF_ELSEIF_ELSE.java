4ff79621d2d95102432eb0c9b621fb9cd8a09bef
Alvaro Castro <arucastro@gmail.com>
2025-04-05 03:02:26 +0200

    Exercise15A_IF_ELSEIF_ELSE FILE CREATED. NOT STARTED

---
+++ 2_Control flow statements_1/src/Exercise16A_IF_ELSEIF_ELSE.java
@@ -0,0 +1,20 @@
+public class Exercise16A_IF_ELSEIF_ELSE {
+    public static void main(String[] args) {
+//        EXERCISE 16A
+//        We have an ice-cream parlour management system that we have to program,
+//        where the user enters the money he has and the system informs
+//        him of the ice cream he can afford. We have of the following ice creams:
+//      􀁸 Water ice cream 60 cents.
+//      􀁸 Cream ice cream 1 euro.
+//      􀁸 Ice cream with toppings 1,6 euros.
+//      􀁸 Ice cream with nutella 1,8 euros.
+//      􀁸 Two scoops of ice cream on a wafer 2 euros.
+//      􀁸 Mini ice cream cake 2,5 euros.
+//
+//        Make different versions of the programme:
+//      􀀹 Show the most expensive ice cream you can afford (maybe none of them).
+//      􀀹 Show the cheapest ice cream you can afford (maybe none of them).
+//      􀀹 Show which of the available ice creams you can afford (maybe none of them)
+
+    }
+}
