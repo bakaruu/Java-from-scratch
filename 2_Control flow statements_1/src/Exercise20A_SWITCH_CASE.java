5dfef3ae7af0e102c2c23a25ecc5faf2b0602d65
Alvaro Castro <arucastro@gmail.com>
2025-04-05 20:33:39 +0200

    Exercise19A_SWITCH_CASE FILE CREATED.

---
+++ 2_Control flow statements_1/src/Exercise20A_SWITCH_CASE.java
@@ -0,0 +1,19 @@
+public class Exercise20A_SWITCH_CASE {
+    public static void main(String[] args) {
+
+//        EXERCISE 20A
+//        Make an application that displays a small menu offering
+//        the possibility to perform 4 operations, asking for the
+//        option number and requesting the necessary operands for
+//        the chosen operation:
+//         [1] Addition: a+b
+//         [2] Subtraction: a-b
+//         [3] Square root: √ (a >0)
+//        Check that a>0 otherwise error
+
+//         [4] Square: a2
+
+//        If the chosen option does not exist, it will exit directly
+//        with an error message.
+    }
+}
