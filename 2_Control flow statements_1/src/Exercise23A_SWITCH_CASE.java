8218aa18379b861e3d6b6951fb230528120f6317
Alvaro Castro <arucastro@gmail.com>
2025-04-06 19:52:12 +0200

    Exercise22A_SWITCH_CASE FILE CREATED.

---
+++ 2_Control flow statements_1/src/Exercise23A_SWITCH_CASE.java
@@ -0,0 +1,22 @@
+public class Exercise23A_SWITCH_CASE {
+    public static void main(String[] args) {
+
+//        EXERCISE 23A
+//        Create a Java application that programs the following menu
+//        of options using the switch-case structure:
+//        1) Calculate and display the VAT (21%) of an amount of euros
+//        entered by keyboard. Define VAT as a variable.
+//        2) Average 3 double numbers entered by keyboard, displaying it
+//        on the screen.
+//        3) Calculate the area of a rectangle given the base and the
+//        height entered by keyboard, showing the result on the screen.
+//         Ask the user for an option (if it does not match any available option,
+//        display an error message and terminate the program) and
+//        carry out the chosen option.
+//         In options 1 and 3, check that the numbers entered by the user are
+//        greater than or equal to 0, otherwise only display an error message.
+
+
+
+    }
+}
