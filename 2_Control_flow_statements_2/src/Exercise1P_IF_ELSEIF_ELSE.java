5ba7e3f77c9d523b4531b062f23ffda0c2e65684
Alvaro Castro <arucastro@gmail.com>
2025-04-06 20:01:54 +0200

    Exercise1P_IF_ELSEIF_ELSE FILE CREATED

---
+++ 2_Control_flow_statements_2/src/Exercise1P_IF_ELSEIF_ELSE.java
@@ -0,0 +1,15 @@
+public class Exercise1P_IF_ELSEIF_ELSE {
+    public static void main(String[] args) {
+
+
+//        EXERCISE 1P Make a program that, given a year, tells us whether
+//        it is a leap year or not. A year is a leap year under the following
+//        conditions:  The year entered must be after 1582, the year in which the
+//        Gregorian calendar we use today was established.  A year divisible
+//        by 4 is a leap year if it is not divisible by 100.  If a year is
+//        divisible by 100 and is also divisible by 400, it is also a leap
+//        year.
+
+
+    }
+}
