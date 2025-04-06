8e5c9bd3be0e8f3dfc4111a71d5c413e34b7eb2e
Alvaro Castro <arucastro@gmail.com>
2025-04-06 19:42:07 +0200

    Exercise20A_SWITCH_CASE DONE.

---
+++ 2_Control flow statements_1/src/Exercise21A_SWITCH_CASE.java
@@ -0,0 +1,25 @@
+public class Exercise21A_SWITCH_CASE {
+    public static void main(String[] args) {
+//        EJERCICIO 21A
+//        Partiendo del programa de los helados vamos a hacer una
+//        versión diferente en la cual se muestran las posibles opciones
+//        de helados y el usuario elegirá cual quiere.
+//
+//        1. Helado de agua 60 céntimos.
+//        2. Helado de crema 1 euro.
+//        3. Helado con toppings 1,6 euros.
+//        4. Helado con nutella 1,8 euros.
+//        5. Dos bolas de helado en barquillo 2 euros.
+//        6. Mini tarta helada 2,5 euros.
+//        Si el usuario ha elegido una opción correcta, el programa
+//        pide que introduzca la cantidad de dinero que tiene el usuario.
+//         El programa sirve el helado mostrando un mensaje:
+//
+//         If, for example, the user enters 2 euros for an ice cream,
+//           the program responds with ‘Your change is: 1€’.
+//         If the money is exact, it will reply with ‘Have a nice day’.
+//         If you don't have enough money ‘Sorry, we don't trust anyone’.
+//           Try to find the clearest and most efficient solution to this
+//           problem by separating input, process and output.
+    }
+}
