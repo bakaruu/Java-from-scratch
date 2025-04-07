66903914d9715fc1d73b0c87f52311333a9ed5a8
Alvaro Castro <arucastro@gmail.com>
2025-04-07 21:15:10 +0200

    Exercise15R_IF_ELSEIF_ELSE DONE

---
+++ 2_Control_flow_statements_2/src/Exercise15R_IF_ELSEIF_ELSE.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Exercise15R_IF_ELSEIF_ELSE {
+    public static void main(String[] args) {
+//        EXERCISE 15R
+//        Given a date expressed in day, month and year, make a programme
+//        that advances this date by one day (do not take into account leap
+//        years and the months are all 30 days).
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter day (1–30): ");
+        int day = scanner.nextInt();
+
+        System.out.print("Enter month (1–12): ");
+        int month = scanner.nextInt();
+
+        System.out.print("Enter year: ");
+        int year = scanner.nextInt();
+
+
+        day++;
+
+        if (day > 30) {
+            day = 1;
+            month++;
+
+            if (month > 12) {
+                month = 1;
+                year++;
+            }
+        }
+
+
+        System.out.printf("📅 Next day: %02d/%02d/%d\n", day, month, year);
+
+    }
+}
