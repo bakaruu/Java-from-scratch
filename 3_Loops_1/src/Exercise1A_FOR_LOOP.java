c15754c39ab2cf771ad6a07fc5e2d33c0924de9d
Alvaro Castro <arucastro@gmail.com>
2025-04-07 22:34:48 +0200

    FOR LOOPS THE BEGINING.

---
+++ 3_Loops_1/src/Exercise1A_FOR_LOOP.java
@@ -0,0 +1,9 @@
+public class Exercise1A_FOR_LOOP {
+    public static void main(String[] args) {
+//        EXERCISE 1A
+//        Develop an application that prompts the user for a letter
+//        and a whole number. This letter will appear as many times
+//        as the user has indicated.
+
+    }
+}
