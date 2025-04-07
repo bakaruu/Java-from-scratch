aa8edd0c7006508f34faf71cb3de5366ecd9c79d
Alvaro Castro <arucastro@gmail.com>
2025-04-07 22:00:11 +0200

    Exercise20R_SWITCH_CASE DONE

---
+++ 2_Control_flow_statements_2/src/Exercise20R_SWITCH_CASE.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Exercise20R_SWITCH_CASE {
+    public static void main(String[] args) {
+//        EXERCISE 20R
+//        Develop an application that prompts the user for a letter.
+//        If the letter read is ‘L’, ‘M’, ‘X’, ‘J’ or ‘V’, the following
+//        will be displayed: ‘The chosen day is a weekday’. If the letter
+//        is ‘S’ or ‘D’, the following message shall be displayed: ‘The
+//        selected day is weekend’. In any other case an error will be
+//        displayed.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a letter representing the day (L, M, X, J, V, S, D): ");
+        char letter = scanner.next().toUpperCase().charAt(0);
+
+        switch (letter) {
+            case 'L':
+            case 'M':
+            case 'X':
+            case 'J':
+            case 'V':
+                System.out.println("The chosen day is a weekday.");
+                break;
+
+            case 'S':
+            case 'D':
+                System.out.println("The selected day is weekend.");
+                break;
+
+            default:
+                System.out.println("❌ Error: Invalid day letter.");
+        }
+    }
+}
