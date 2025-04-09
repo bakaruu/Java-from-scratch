659f569a2cf9f3405c71be66ad77e40b33ba30bf
Alvaro Castro <arucastro@gmail.com>
2025-04-09 18:25:05 +0200

    Exercise22A_WHILE_DOWHILE.

---
+++ 3_Loops_1/src/Exercise22A_WHILE_DOWHILE.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercise22A_WHILE_DOWHILE {
+    public static void main(String[] args) {
+//        EXERCISE 22A
+//        Make a program that asks for numbers until a positive integer
+//        is entered.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        do{
+            System.out.println("Enter a number: ");
+            number = scanner.nextInt();
+        }while(number<0);
+    }
+}
