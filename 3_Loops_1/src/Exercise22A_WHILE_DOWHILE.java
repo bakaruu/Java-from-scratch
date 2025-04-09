ad003fefe68f348beb2210b45deb776ec82b1aa1
Alvaro Castro <arucastro@gmail.com>
2025-04-09 19:14:54 +0200

    Exercise22A_WHILE_DOWHILE DONE.

--- 3_Loops_1/src/Exercise22A_WHILE_DOWHILE.java
+++ 3_Loops_1/src/Exercise22A_WHILE_DOWHILE.java
@@ -13,5 +13,7 @@ public class Exercise22A_WHILE_DOWHILE {
             System.out.println("Enter a number: ");
             number = scanner.nextInt();
         }while(number<0);
+        //DONE
+
     }
 }
