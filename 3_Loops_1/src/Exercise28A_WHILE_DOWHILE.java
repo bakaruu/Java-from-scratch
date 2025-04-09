66489f1c78f94844919cffb7b9687cb2771abe71
Alvaro Castro <arucastro@gmail.com>
2025-04-09 19:10:46 +0200

    Exercise28A_WHILE_DOWHILE.

---
+++ 3_Loops_1/src/Exercise28A_WHILE_DOWHILE.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercise28A_WHILE_DOWHILE {
+    public static void main(String[] args) {
+//        EXERCISE 28A
+//        Write a program that reads an integer (positive or negative) until the
+//        following 3 conditions are met:
+
+//         Let it be even.
+//         Ending in 2.
+//         It must have at least two digits.
+//        If it does it on the first try, display ‘You're a crack’, if it took 5
+//        or more, display ‘It cost you’. Otherwise, nothing is displayed
+//        on the screen.
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+        int attempts = 0;
+
+        do {
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+            attempts++;
+        } while (!(number % 2 == 0 && Math.abs(number) % 10 == 2 && Math.abs(number) >= 10));
+
+//        Check how many attempts it took
+        if (attempts == 1) {
+            System.out.println("You're a crack");
+        } else if (attempts >= 5) {
+            System.out.println("It cost you");
+        }
+    }
+}
