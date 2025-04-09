b886b5b182427f2d41c1186c0a588d7fddfbe8ce
Alvaro Castro <arucastro@gmail.com>
2025-04-09 19:03:54 +0200

    Exercise27A_WHILE_DOWHILE.

---
+++ 3_Loops_1/src/Exercise27A_WHILE_DOWHILE.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercise27A_WHILE_DOWHILE {
+    public static void main(String[] args) {
+//        EXERCISE 27A
+//        Repeat the previous exercises but using the do-while loop.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many numbers will you enter? ");
+        int n = scanner.nextInt();
+
+        boolean foundZero = true;
+        int count = 0;
+
+        do {
+            System.out.print("Enter number " + (count + 1) + ": ");
+            int number = scanner.nextInt();
+
+            if (number == 0) {
+                foundZero = false;
+                System.out.println("Zero number detected.");
+            }
+
+            count++;
+        }while (count < n && foundZero);
+    }
+}
