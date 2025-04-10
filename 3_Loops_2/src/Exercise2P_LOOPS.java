3544129012ea758d25cb6f8150afb22436812262
Alvaro Castro <arucastro@gmail.com>
2025-04-10 20:30:31 +0200

    Exercise1P_LOOPS FILE CREATED.

---
+++ 3_Loops_2/src/Exercise2P_LOOPS.java
@@ -0,0 +1,14 @@
+public class Exercise2P_LOOPS {
+    public static void main(String[] args) {
+//        EXERCISE 2P
+//        We want to stack a certain number of tin cans in the shape of a pyramid so
+//        that at the top there is only one can, in the row above there will be two cans,
+//        in the row below there will be 3 cans and so on. Make a program that reads
+//        the number of cans we have and says whether it is possible to stack them in
+//        this way or not.
+//        For example, 6 cans would be stackable (1+2+3), 10 cans would be
+//        stackable (1+2+3+4), 9 cans would not be possible.
+
+
+    }
+}
