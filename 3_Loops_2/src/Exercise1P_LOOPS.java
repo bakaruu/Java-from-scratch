94dbf8ee6e2684db54aebe39116467f57f3430f5
Alvaro Castro <arucastro@gmail.com>
2025-04-10 20:20:57 +0200

    Exercise1P_LOOPS.

---
+++ 3_Loops_2/src/Exercise1P_LOOPS.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercise1P_LOOPS {
+    public static void main(String[] args) {
+//        EXERCISE 1P
+//        A number is perfect if it is equal to the sum of all its positive
+//        divisors not including the number itself. For example, the number
+//    6 is perfect because it has divisors 1, 2, 3 and 6, but 6 is not counted
+//        as a divisor to check if it is perfect. If we add 1 + 2 + 3 = 6.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number to check if is perfect: ");
+        int number = scanner.nextInt();
+
+
+    }
+}
