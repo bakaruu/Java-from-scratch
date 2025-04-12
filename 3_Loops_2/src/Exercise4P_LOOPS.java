58a8a25a398286b51bc7cb5c8b28046a49ef5f11
Alvaro Castro <arucastro@gmail.com>
2025-04-12 20:02:12 +0200

    Exercise3P_LOOPS FILE CREATED.

---
+++ 3_Loops_2/src/Exercise4P_LOOPS.java
@@ -0,0 +1,12 @@
+public class Exercise4P_LOOPS {
+
+    public static void main(String[] args) {
+//       EXERCISE 4P
+//       Create a program that determines when two integers are friends.
+//       Two friends are two positive integers a and b such that a is the sum
+//       of the proper divisors of b (excluding 1 and b itself), and b is the
+//       sum of the proper divisors of a (excluding 1 and a itself).
+
+
+    }
+}
