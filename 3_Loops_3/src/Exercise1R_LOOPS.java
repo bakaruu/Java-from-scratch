f51a1576f232153c79a69e1702f90099c0f69cb3
Alvaro Castro <arucastro@gmail.com>
2025-04-13 19:43:25 +0200

    Exercise1R_LOOPS DONE

---
+++ 3_Loops_3/src/Exercise1R_LOOPS.java
@@ -0,0 +1,13 @@
+public class Exercise1R_LOOPS {
+    public static void main(String[] args) {
+//        EXERCISE 1R
+//        Make a program that prints the terms of the series 11, 22, 33, 44, up to
+//    275 etc. (The values are not entered on the keyboard, they must be generated
+//        by means of a for loop).
+
+        for(int i=11; i<=275; i+=11){
+
+            System.out.println(i);
+        }
+    }
+}
