e73d4238436e8b7fb4f3fa57761dbf2dfb12e99b
Alvaro Castro <arucastro@gmail.com>
2025-04-13 16:05:43 +0200

    Exercise4P_LOOPS DONE

--- 3_Loops_2/src/Exercise4P_LOOPS.java
+++ 3_Loops_2/src/Exercise4P_LOOPS.java
@@ -1,11 +1,40 @@
+import java.util.Scanner;
+
 public class Exercise4P_LOOPS {
 
     public static void main(String[] args) {
 //       EXERCISE 4P
 //       Create a program that determines when two integers are friends.
 //       Two friends are two positive integers a and b such that a is the sum
-//       of the proper divisors of b (excluding 1 and b itself), and b is the
-//       sum of the proper divisors of a (excluding 1 and a itself).
+//       of the proper divisors of b (excluding b itself), and b is the
+//       sum of the proper divisors of a (excluding a itself).
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = scanner.nextInt();
+
+        System.out.println("Enter first number: ");
+        int b = scanner.nextInt();
+
+        int sumA= 0;
+        int sumB= 0;
+
+        for (int i= 1; i<a; i++){
+            if (a % i == 0) {
+                sumA+=i;
+            }
+        }
+
+        for (int i= 1; i<b; i++){
+            if (b % i == 0) {
+                sumB+=i;
+            }
+        }
+
+        if(sumA == b && sumB == a){
+            System.out.println("Both numbers are friends.");
+        }
+
 
 
     }
