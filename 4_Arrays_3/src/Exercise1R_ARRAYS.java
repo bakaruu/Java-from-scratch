54eb8cc25402625d087e23c1c07102077f759d0a
Alvaro Castro <arucastro@gmail.com>
2025-04-16 20:18:09 +0200

    Exercise1R_ARRAYS FILE CREATED

---
+++ 4_Arrays_3/src/Exercise1R_ARRAYS.java
@@ -0,0 +1,8 @@
+public class Exercise1R_ARRAYS {
+    public static void main(String[] args) {
+//        EXERCISE 1R
+//        Develop a program that, given an array of integers, displays the sum of
+//        all its elements squared. For example:
+//        With the array [1, 2, 2] it should display 9 because 12 + 22 + 22 = 9.
+    }
+}
