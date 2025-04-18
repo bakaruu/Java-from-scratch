1370952e6b4b1a744fc832ea5df314603d02bbe4
Alvaro Castro <arucastro@gmail.com>
2025-04-18 17:51:49 +0200

    Exercise1R_ARRAYS DONE

--- 4_Arrays_3/src/Exercise1R_ARRAYS.java
+++ 4_Arrays_3/src/Exercise1R_ARRAYS.java
@@ -4,5 +4,16 @@ public class Exercise1R_ARRAYS {
 //        Develop a program that, given an array of integers, displays the sum of
 //        all its elements squared. For example:
 //        With the array [1, 2, 2] it should display 9 because 12 + 22 + 22 = 9.
+
+        int[] arr = {1, 2, 2};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i] * arr[i];
+        }
+
+        System.out.println("Sum of squares: " + sum);
     }
+
+
 }
